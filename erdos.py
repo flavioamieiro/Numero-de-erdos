3200a043ecdd107547cf0001360eccc81a411484
Flavio Amieiro <amieiro.flavio@gmail.com>
2011-02-15 23:31:46 -0200

    Guarda informações de co-autores no Nó

--- erdos.py
+++ erdos.py
@@ -4,6 +4,7 @@ INFINITO = sys.maxsize
 
 class No:
     def __init__(self, nome, numero=None):
+        self.co_autores = set()
         self.nome = nome
         self._numero = numero
         self.pai = None
@@ -18,6 +19,10 @@ class No:
             else:
                 self._numero = INFINITO
         return self._numero
+
+    def update_co_autores(self, nos):
+        self.co_autores.update(nos)
+        self.co_autores.discard(self)
 
     def perfilhar_nos_em(self, nos):
         for no in nos:
@@ -36,6 +41,9 @@ class No:
     def __repr__(self):
         return '{0} - {1}'.format(self.nome, self.numero)
 
+    def __hash__(self):
+        return hash(self.nome)
+
 class Erdos(dict):
 
     def __init__(self, livros):
@@ -44,6 +52,7 @@ class Erdos(dict):
         for livro in livros:
             nos = list(map(lambda x: self.get(x, No(x, INFINITO)), livro))
             for no in nos:
+                no.update_co_autores(nos)
                 self[no.nome] = no
 
             min(nos).perfilhar_nos_em(nos)
